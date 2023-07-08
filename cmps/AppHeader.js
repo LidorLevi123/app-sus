ce1d1138b54ead207541b8d0147e4a75efb2fa05
StrukovSergei <strukov.sergei.999@gmail.com>
2023-07-08 18:16:02 +0300

    added hover to menu and change randomcolor function

--- cmps/AppHeader.js
+++ cmps/AppHeader.js
@@ -24,7 +24,7 @@ export default {
   data() {
     return {
       showMenu: false
-    };
+    }
   },
   methods: {
     goTo(route) {
--- services/util.service.js
+++ services/util.service.js
@@ -79,10 +79,10 @@ function animateCSS(el, animation) {
 }
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+    var letters = 'BCDEF'.split('')
+    var color = '#'
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * letters.length)]
     }
-    return color;
+    return color
 }
