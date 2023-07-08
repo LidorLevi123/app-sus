8861a8e4c85cee8772d276df668d732ce866be09
StrukovSergei <strukov.sergei.999@gmail.com>
2023-07-08 14:52:46 +0300

    added aboutus page

--- views/AboutUs.js
+++ views/AboutUs.js
@@ -1,7 +1,31 @@
 export default {
 	template: `
         <section class="about-page">
-            <h1>About us</h1>
-        </section>
+            <h1 class="about-title">About us</h1>
+        
+        <div class="about-container">
+            <div class="about-card">
+                <h2>Lidor</h2>
+    <div class="about-image1">
+      <img src="https://robohash.org/lidor.png" alt="Image 1">
+      <p class="about-caption">Working with Sergei was an absolute pleasure.
+         His dedication and expertise brought a unique perspective to our project.
+          Together, we seamlessly combined our skills and ideas, resulting in a truly remarkable outcome that exceeded all expectations.</p>
+          <a title="github link" class="github-link" href="https://github.com/LidorLevi123"><img src="assets/img/github.png" alt="GitHub"></a>
+    </div>
+    </div>
+    <div class="about-card">
+        <h2>Sergei</h2>
+    <div class="about-image2">
+      <img src="https://robohash.org/sergei.png" alt="Image 2">
+      <p class="about-caption">It was an incredible experience collaborating with Lidor on this project.
+         His creativity and attention to detail added immense value to our work.
+          Throughout the process, we fostered a strong synergy, inspiring each other to push boundaries and achieve outstanding results.
+           I'm grateful for the opportunity to have worked alongside such a talented professional.</p>
+           <a title="github link" class="github-link" href="https://github.com/StrukovSergei"><img src="assets/img/github.png" alt="GitHub"></a>
+    </div>
+  </div>
+  </div>
+  </section>
     `
 }
